bb1fb4e5b8041b3356d743756240bc864f526ba6
Jeongwoo Park <wahoo9040@naver.com>
2026-01-02 20:20:49 +0900

    feat: add refresh tokens migration and entity implementation, update dependencies

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20241210_000001_create_users_table;
 mod m20241220_000001_create_memos_table;
 mod m20241222_000001_add_oauth_accounts;
+mod m20250102_000001_create_refresh_tokens;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20241210_000001_create_users_table::Migration),
             Box::new(m20241220_000001_create_memos_table::Migration),
             Box::new(m20241222_000001_add_oauth_accounts::Migration),
+            Box::new(m20250102_000001_create_refresh_tokens::Migration),
         ]
     }
 }
